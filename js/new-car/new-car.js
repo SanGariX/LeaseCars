ed85cd7ce04d63ca3abc8da669cca31a8358be6b
SanGarix <denya.garkusha.12@gmail.com>
2024-05-21 20:12:03 +0300

    0.1.0

--- js/new-car/new-car.js
+++ js/new-car/new-car.js
@@ -1,6 +1,1 @@
-const menuBtn = document.querySelector(".open__header__btn");
-const menu = document.querySelector(".header__list");
 
-menuBtn.addEventListener("click", () => {
-  menu.classList.toggle("header__list--active");
-});
